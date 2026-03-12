ca1c6059e76d6dd6999e4bb4856a54e6098e3414
ZyaD-Mostafa <zyadmostafa9011@gmail.com>
2026-03-12 22:05:46 +0200

    del

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  All,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Req,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserRepository } from './DB/Repositories/user.repo';
 
-@Controller('cats')
+@Controller('')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly _userRepo: UserRepository,
+  ) {}
+  @Delete('del-acc/:email')
+  async deleteAccount(@Param('email') email: string) {
+    const result = await this._userRepo.deleteOne({ email });
 
-  @Get('getall')
-  getAll(): string {
-    return 'Get all cats';
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('User not found');
+    }
+
+    return { message: 'User deleted successfully' };
   }
-  @Get()
-  home(): string {
-    return 'Home';
+  @All('/*dummy')
+  dummy(@Req() req: Request) {
+    return {
+      message: `url ${req.url} not found`,
+    };
   }
 }
