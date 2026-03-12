22a68185d258212f0bf4b2a0d49e7a341088dd5e
ZyaD-Mostafa <zyadmostafa9011@gmail.com>
2026-03-12 21:44:23 +0200

    test

---
+++ src/common/utils/Cors/cors.ts
@@ -0,0 +1,23 @@
+export function corsOption() {
+  const whiteList = process.env.WHITELIST_URLS?.split(',') || [];
+
+  const corsOptions = {
+    origin: function (
+      origin: string,
+      callback: (error: Error | null, allow?: boolean) => void,
+    ) {
+      if (whiteList.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+  return {
+    origin: whiteList,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  return corsOptions;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
+import { corsOption } from './common/utils/Cors/cors';
 async function bootstrap() {
   const port = process.env.PORT || 447;
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOption());
   app.useGlobalInterceptors(new ResponseInterceptor());
   //app.use('/uploads', express.static('./src/uploads'));
   await app.listen(port, () => {
